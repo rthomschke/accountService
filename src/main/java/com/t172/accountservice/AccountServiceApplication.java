5cb904a5e42582b912fc60f2fd2c069a72e96e41
Rene Thomschke <Rene.Thomschke@kfw.de>
2024-12-11 18:04:13 +0100

    Fixed openapi generate

--- src/main/java/com/t172/accountservice/AccountServiceApplication.java
+++ src/main/java/com/t172/accountservice/AccountServiceApplication.java
@@ -1,10 +1,11 @@
 package com.t172.accountservice;
 
+import org.openapitools.api.AccountApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountServiceApplication {
+public class AccountServiceApplication implements AccountApi {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
